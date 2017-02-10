628520ebfe38381fa7ed4e1a2d5045b50a3023f1
Mario <Maslz980outlook.com>
2017-02-10 14:17:25 -0600

    Detalles de main y funcion

--- Lab4.cpp
+++ Lab4.cpp
@@ -2,9 +2,42 @@
 
 using namespace std;
 
-int cXOR();
+int cXOR(string palClave, string texto);
 
 int main(){
-	
+	char seguir = 's';
+	while(resp =='s' || resp == 'S'){
+		cout<<"1. Cifrar \n2. Decifrar"<<endl;
+       		int ej;
+	        cin>>ej;
+		switch(ej){
+			case 1:{
+				
+				break;
+			}
+			case 2:{
+				
+				break;
+			}
+		}
+	}
 	return 0;
+}
+
+int cXOR(string palClave, string texto){
+	int tamPal;
+	tamPal = palClave.length;
+	int tamTex;
+	tamTex = texto.length;
+	int* arrePal = new int[tamPal];
+	int* arreTex = new int[tamTex];
+	for(int i =0; i<tamPal; i++){
+		int temp = palClave.at(i);
+		arrePal[i] = temp;
+	}
+	for(int i =0; i< tamTex;i++){
+		int temp = palTex.at(i);
+		arreTex[i] = temp;
+	}
+
 }
