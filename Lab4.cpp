a1857543280383b0dd557be81b438e46b3a68d47
isaiasm-hn <isaiasm_hn@unitec.edu>
2017-02-10 16:40:01 -0600

    Vigenere finalizado!

--- Lab4.cpp
+++ Lab4.cpp
@@ -4,8 +4,9 @@
 
 using namespace std;
 
-
-char* Vigenere();
+string abcedariofrom(int);
+int abecedario(string);
+string* Vigenere(string,int);
 int cXOR();
 char* abcdario(); 
 
@@ -20,6 +21,7 @@ int main(){
 
 	char resp = 's';
 	while(resp=='s' || resp == 'S'){
+		cout<<endl;
 		cout<<"1. Cifrar \n2. Decifrar"<<endl;
        		int ej;
 	        cin>>ej;
@@ -33,8 +35,12 @@ int main(){
 				cin>>mensaje;	
 				cout<<p_clave<<endl<<mensaje;
 				int tam_clave=p_clave.length();
+	         cout<<endl;
+				cout<<"-------Cifrado de Vigenere------"<<endl;
+				string* vigenere=NULL;			
+      	 	vigenere=Vigenere(p_clave,tam_clave);
+				cout<<"El texto cifrado en Vigenere\n"<<vigenere[0];
 				
-      	 
 	
 				break;
 			}
@@ -48,130 +54,246 @@ int main(){
 }
 
 
+int abecedario(char h) {
+        
+		switch (h) {
+            case 'a': 
+			
+					return 1;
+				
+            case 'b':
 
+                return 2;
 
+            case 'c':
 
+                return 3;
 
+            case 'd':
 
+                return 4;
 
+            case 'e':
 
+                return 5;
 
+            case 'f':
 
+                return 6;
 
+            case 'g':
 
+                return 7;
 
+            case 'h':
 
+                return 8;
 
+            case 'i':
 
+                return 9;
 
+            case 'j':
 
+                return 10;
 
+            case 'k':
 
+                return 11;
 
+            case 'l':
 
+                return 12;
 
+            case 'm':
 
+                return 13;
 
+            case 'n':
 
+                return 14;
 
+            case 'o':
 
+                return 15;
 
+            case 'p':
 
+                return 16;
 
+            case 'q':
 
+                return 17;
 
+            case 'r':
 
+                return 18;
 
+            case 's':
 
+                return 19;
 
+            case 't':
 
+                return 20;
 
+            case 'u':
 
+                return 21;
 
+            case 'v':
 
+                return 22;
 
+            case 'w':
 
+                return 23;
 
+            case 'x':
 
+                return 24;
 
+            case 'y':
 
+                return 25;
 
+            case 'z':
 
+                return 26;
+	}
+}
+string abcedariofrom(int x) {
 
+        switch (x) {
 
+            case 1:
 
+                return "a";
 
+            case 2:
 
+                return "b";
 
+            case 3:
 
+                return "c";
 
+            case 4:
 
+                return "d";
 
+            case 5:
 
+                return "e";
 
+            case 6:
 
+                return "f";
 
+            case 7:
 
+                return "g";
 
+            case 8:
 
+                return "h";
 
+            case 9:
 
+                return "i";
 
+            case 10:
 
+                return "j";
 
+            case 11:
 
+                return "k";
 
+            case 12:
 
+                return "l";
 
+            case 13:
 
+                return "m";
 
+            case 14:
 
+                return "n";
 
+            case 15:
 
+                return "o";
 
+            case 16:
 
+                return "p";
 
+            case 17:
 
+                return "q";
 
+            case 18:
 
+                return "r";
 
+            case 19:
 
+                return "s";
 
+            case 20:
 
+                return "t";
 
+            case 21:
 
+                return "u";
 
+            case 22:
 
+                return "v";
 
+            case 23:
 
+                return "w";
 
+            case 24:
 
+                return "x";
 
+            case 25:
 
+                return "y";
 
+            case 26:
 
+                return "z";
 
+             }
 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-char* Vigenere(){
+string* Vigenere(string cad, int clave){
+	string* retVal=new string[1];
 	
+	string vig="";
+   int pos;
+	int cont=1; 
+	for(int i=0;i<cad.length(); i++){
+			if(cad.at(i) != ' '){
+				pos=abecedario(cad.at(i)) + cont;
+				vig+=abcedariofrom(pos);
+			}else{
+				vig+=" ";
+			}if(cont==clave){
+				cont=1;
+			}else{
+				cont++;
+			}					
+	}
+	retVal[0]=vig;
+	return retVal;
 }
 
 
